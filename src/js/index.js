f51fcb3b6209bb30b2dd4ca8a1181c7401fd640f
postgre <819186192@qq.com>
2021-09-17 12:15:38 +0800

    try frontend
    right with html: including:
        switch identity and anchor
    but when applying to render(),fails

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@ import './../css/index.css'
 
 class App extends React.Component {
     constructor(props){
+        super(props)
         this.state = {
             InjectionID:0
         }
@@ -16,15 +17,444 @@ class App extends React.Component {
             console.log("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
             this.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
          }
+         const MyContract = web3.eth.contract([
+            {
+                "constant": true,
+                "inputs": [],
+                "name": "AuthorityGetUncheckedInjectionList",
+                "outputs": [
+                    {
+                        "name": "flist",
+                        "type": "uint256[]"
+                    },
+                    {
+                        "name": "cnt",
+                        "type": "uint256"
+                    },
+                    {
+                        "name": "totalCnt",
+                        "type": "uint256"
+                    },
+                    {
+                        "name": "herenumbersinWaitingList",
+                        "type": "uint256"
+                    }
+                ],
+                "payable": false,
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "constant": true,
+                "inputs": [],
+                "name": "VaccinatedOneGetList",
+                "outputs": [
+                    {
+                        "name": "list",
+                        "type": "uint256[]"
+                    }
+                ],
+                "payable": false,
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "constant": false,
+                "inputs": [
+                    {
+                        "name": "addr",
+                        "type": "address"
+                    }
+                ],
+                "name": "grantHospitals",
+                "outputs": [],
+                "payable": false,
+                "stateMutability": "nonpayable",
+                "type": "function"
+            },
+            {
+                "constant": true,
+                "inputs": [],
+                "name": "testt",
+                "outputs": [
+                    {
+                        "name": "n",
+                        "type": "uint256"
+                    },
+                    {
+                        "name": "id",
+                        "type": "uint8"
+                    }
+                ],
+                "payable": false,
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "constant": true,
+                "inputs": [],
+                "name": "VaccinatedOneGetHisPassport",
+                "outputs": [
+                    {
+                        "name": "_ID",
+                        "type": "address"
+                    },
+                    {
+                        "name": "_name",
+                        "type": "string"
+                    },
+                    {
+                        "name": "_totalStatus",
+                        "type": "uint8"
+                    }
+                ],
+                "payable": false,
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "constant": true,
+                "inputs": [
+                    {
+                        "name": "i",
+                        "type": "uint256"
+                    }
+                ],
+                "name": "showSpecificInjection",
+                "outputs": [
+                    {
+                        "name": "_InjectionID",
+                        "type": "uint256"
+                    },
+                    {
+                        "name": "_kind",
+                        "type": "string"
+                    },
+                    {
+                        "name": "_ID",
+                        "type": "address"
+                    },
+                    {
+                        "name": "_date",
+                        "type": "string"
+                    },
+                    {
+                        "name": "_injectedIndex",
+                        "type": "uint256"
+                    },
+                    {
+                        "name": "_InjectionStatus",
+                        "type": "uint8"
+                    }
+                ],
+                "payable": false,
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "constant": false,
+                "inputs": [
+                    {
+                        "name": "addr",
+                        "type": "address"
+                    },
+                    {
+                        "name": "_name",
+                        "type": "string"
+                    }
+                ],
+                "name": "deliverPassport",
+                "outputs": [],
+                "payable": false,
+                "stateMutability": "nonpayable",
+                "type": "function"
+            },
+            {
+                "constant": true,
+                "inputs": [
+                    {
+                        "name": "addr",
+                        "type": "address"
+                    }
+                ],
+                "name": "LookUp",
+                "outputs": [
+                    {
+                        "name": "ID",
+                        "type": "address"
+                    },
+                    {
+                        "name": "name",
+                        "type": "string"
+                    },
+                    {
+                        "name": "injectedIndex",
+                        "type": "uint256"
+                    },
+                    {
+                        "name": "totalStatus",
+                        "type": "uint8"
+                    }
+                ],
+                "payable": false,
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "constant": false,
+                "inputs": [
+                    {
+                        "name": "i",
+                        "type": "uint256"
+                    },
+                    {
+                        "name": "proposal",
+                        "type": "uint8"
+                    }
+                ],
+                "name": "AuthorityDisposeUncheckedInjection",
+                "outputs": [],
+                "payable": false,
+                "stateMutability": "nonpayable",
+                "type": "function"
+            },
+            {
+                "constant": false,
+                "inputs": [
+                    {
+                        "name": "addr",
+                        "type": "address"
+                    },
+                    {
+                        "name": "totalStatus",
+                        "type": "uint8"
+                    }
+                ],
+                "name": "AuthorityChangeToatalStatus",
+                "outputs": [],
+                "payable": false,
+                "stateMutability": "nonpayable",
+                "type": "function"
+            },
+            {
+                "constant": false,
+                "inputs": [
+                    {
+                        "name": "authorityAddr",
+                        "type": "address"
+                    },
+                    {
+                        "name": "kind",
+                        "type": "string"
+                    },
+                    {
+                        "name": "ID",
+                        "type": "address"
+                    },
+                    {
+                        "name": "date",
+                        "type": "string"
+                    }
+                ],
+                "name": "HospitalSubmitInfomation",
+                "outputs": [
+                    {
+                        "name": "success",
+                        "type": "bool"
+                    }
+                ],
+                "payable": false,
+                "stateMutability": "nonpayable",
+                "type": "function"
+            },
+            {
+                "inputs": [],
+                "payable": false,
+                "stateMutability": "nonpayable",
+                "type": "constructor"
+            },
+            {
+                "anonymous": false,
+                "inputs": [
+                    {
+                        "indexed": false,
+                        "name": "addr",
+                        "type": "address"
+                    }
+                ],
+                "name": "LookUpEvent",
+                "type": "event"
+            },
+            {
+                "anonymous": false,
+                "inputs": [
+                    {
+                        "indexed": false,
+                        "name": "addr",
+                        "type": "address"
+                    },
+                    {
+                        "indexed": false,
+                        "name": "name",
+                        "type": "string"
+                    }
+                ],
+                "name": "deliverPassportEvent",
+                "type": "event"
+            },
+            {
+                "anonymous": false,
+                "inputs": [
+                    {
+                        "indexed": false,
+                        "name": "addr",
+                        "type": "address"
+                    }
+                ],
+                "name": "grantHospitalEvent",
+                "type": "event"
+            },
+            {
+                "anonymous": false,
+                "inputs": [
+                    {
+                        "indexed": false,
+                        "name": "auth",
+                        "type": "address"
+                    },
+                    {
+                        "indexed": false,
+                        "name": "kind",
+                        "type": "string"
+                    },
+                    {
+                        "indexed": false,
+                        "name": "ID",
+                        "type": "address"
+                    },
+                    {
+                        "indexed": false,
+                        "name": "date",
+                        "type": "string"
+                    }
+                ],
+                "name": "HospitalSubmitInfomationEvent",
+                "type": "event"
+            },
+            {
+                "anonymous": false,
+                "inputs": [],
+                "name": "AuthorityGetUncheckedInjectionListEvent",
+                "type": "event"
+            },
+            {
+                "anonymous": false,
+                "inputs": [
+                    {
+                        "indexed": false,
+                        "name": "i",
+                        "type": "uint256"
+                    },
+                    {
+                        "indexed": false,
+                        "name": "proposal",
+                        "type": "uint8"
+                    }
+                ],
+                "name": "AuthorityDisposeUncheckedInjectionEvent",
+                "type": "event"
+            },
+            {
+                "anonymous": false,
+                "inputs": [
+                    {
+                        "indexed": false,
+                        "name": "addr",
+                        "type": "address"
+                    },
+                    {
+                        "indexed": false,
+                        "name": "totalStatus",
+                        "type": "uint8"
+                    }
+                ],
+                "name": "AuthorityChangeToatalStatusEvent",
+                "type": "event"
+            },
+            {
+                "anonymous": false,
+                "inputs": [],
+                "name": "VaccinatedOneGetHisPassportEvent",
+                "type": "event"
+            },
+            {
+                "anonymous": false,
+                "inputs": [],
+                "name": "VaccinatedOneGetListEvent",
+                "type": "event"
+            },
+            {
+                "anonymous": false,
+                "inputs": [
+                    {
+                        "indexed": false,
+                        "name": "i",
+                        "type": "uint256"
+                    }
+                ],
+                "name": "showSpecificInjectionEvent",
+                "type": "event"
+            }
+        ])
+         this.state.ContractInstance = MyContract.at("0x21Af7f8637686EA86A997E9276F820e59e814a17")
 
+         window.a = this.state
 
     }
+    
     render(){
-        return (
-            
+        return(
+            <div>
+                <p> try </p>
+                {/* <div id="AuthorityPart" style="display: none;">  */}
+                <div id="AuthorityPart">
+                    <h2>Here you are Authority</h2>
+                    <nav>       
+                        <a href="#DeliverPassport">DeliverPassport</a>
+                        <a href="#ChangeTotalStatus">ChangeTotalStatus</a>
+                    </nav>
+                    <section>
+                        <div id="DeliverPassport">
+                            DeliverPassport here
+                        </div>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <div id="ChangeTotalStatus">
+                            ChangeTotalStatus here
+                        </div>
+                    </section>
+                </div>
+                <div id="HospitalPart" style="display: none;">
+                    <h2>Here you are Hospital</h2>
+                </div>
+                <div id="UserPart" style="display: none;">
+                    <h2>Here you are User</h2>
+                </div>
+            </div>
         )
     }
+    
 }
+
 ReactDOM.render(
     <App />,
     document.querySelector('#root')
